9552fcb52fe733bfd4eb59518fcb7e7d655bed55
Gregory Luneau <gregory.luneau@gmail.com>
2023-11-01 10:15:40 -0500

    fix export seed message

--- packages/snap/src/rpc/exportSeed.ts
+++ packages/snap/src/rpc/exportSeed.ts
@@ -6,19 +6,15 @@ const astarCoinType = 810;
 export async function exportSeed(): Promise<string | null> {
   // ask for confirmation
   const confirmation = await showConfirmationDialog({
-    prompt: `Are you sure you want to export your seed? 
-    Please be aware that exporting your seed poses significant risks:
-
+    prompt: `Are you sure you want to export your seed?`,
+    description: `Please be aware that exporting your seed poses significant risks!
     - If your seed is exposed to others, they can gain full access to your funds.
     - A leaked seed can lead to a total loss of all your assets in this account.
-  
     To ensure the security of your funds:
-    
     - Store your seed in a secure, offline location.
     - Do not share your seed with anyone.
     - Avoid storing it on digital devices that can be hacked or accessed without your consent.
-  
-    By exporting your seed, you acknowledge these risks and the responsibility for keeping it safe. 
+    By exporting your seed, you acknowledge these risks and the responsibility for keeping it safe.
     Do you still wish to proceed?`
   });
   // return seed if user confirmed action
