be55497230b5f6c04796144ee9a22e7e1f145d60
codespool <4625220+codespool@users.noreply.github.com>
2025-07-03 11:15:48 +0300

    update confirmation ui

---
+++ packages/snap/src/util/confirmation.tsx
@@ -0,0 +1,30 @@
+import { Box, Heading, Text  } from '@metamask/snaps-sdk/jsx';
+
+type ConfirmationDialogContent = {
+  prompt: string;
+  description?: string;
+  textAreaContent?: string;
+};
+
+export async function showConfirmationDialog(message: ConfirmationDialogContent): Promise<boolean> {
+  return (await snap.request({
+    method: 'snap_dialog',
+    params: {
+      content: (
+        <Box>
+          <Heading>{message.prompt || 'Are you sure?'}</Heading>
+          {<Text>{message.description || ''}</Text>}
+          {<Text>{message.textAreaContent || ''}</Text>}
+        </Box>
+      ),
+      type: 'confirmation'
+    }
+  })) as boolean;
+}
+
+
+// panel([
+//         heading(message.prompt || 'Are you sure?'),
+//         text(message.description || ''),
+//         text(message.textAreaContent || '')
+//       ]),
